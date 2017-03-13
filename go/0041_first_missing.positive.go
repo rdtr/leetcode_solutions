fa87646a35b3cea51a9853ffa8ccd2cf8d970090
Norio Akagi <redtree.dev1112@gmail.com>
2017-03-13 09:15:22 -0700

    commit 0041 in Go

---
+++ go/0041_first_missing.positive.go
@@ -0,0 +1,16 @@
+func firstMissingPositive(nums []int) int {
+	nlen := len(nums)
+	for i := 0; i < nlen; i++ {
+		if nums[i] >= 1 && nums[i] <= nlen && nums[i] != nums[nums[i]-1] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+			i-- // keep the same index
+		}
+	}
+
+	for i := 0; i < nlen; i++ {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+	return nlen + 1
+}
