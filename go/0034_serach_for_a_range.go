de21f754b7efa3c1ccefdab670aaa9ca9834396a
Norio Akagi <redtree.dev1112@gmail.com>
2017-06-09 01:29:46 -0700

    update 0034

--- go/0034_serach_for_a_range.go
+++ go/0034_serach_for_a_range.go
@@ -1,64 +1,34 @@
-package main
-
 func searchRange(nums []int, target int) []int {
 	nlen := len(nums)
 	if nlen == 0 {
 		return []int{-1, -1}
-	} else if nlen == 1 {
-		if target == nums[0] {
-			return []int{0, 0}
+	}
+	lo, hi := 0, nlen-1
+	var mid int
+
+	// search left
+	for lo < hi && lo >= 0 && hi < nlen {
+		if mid = lo + (hi-lo)/2; nums[mid] < target {
+			lo = mid + 1
 		} else {
-			return []int{-1, -1}
+			hi = mid
 		}
 	}
-
-	left, right := searchLeft(nums, target), searchRight(nums, target)
-	if left == -1 && right == -1 {
+	if lo < 0 || hi >= nlen || nums[lo] != target {
 		return []int{-1, -1}
 	}
+	left := hi
+
+	//search right
+	lo, hi = left, nlen-1
+	for lo < hi && lo >= 0 && hi < nlen {
+		if mid = hi - (hi-lo)/2; nums[mid] <= target {
+			lo = mid
+		} else {
+			hi = mid - 1
+		}
+	}
+	right := lo
+
 	return []int{left, right}
-}
-
-func searchLeft(nums []int, target int) int {
-	left, right := 0, len(nums)-1
-	for right-left > 1 {
-		mid := (left + right) / 2
-		if nums[mid] < target && nums[mid+1] == target {
-			return mid + 1
-		} else if nums[mid] < target {
-			left = mid
-		} else {
-			right = mid
-		}
-	}
-
-	if nums[left] == target {
-		return left
-	}
-	if nums[right] == target {
-		return right
-	}
-	return -1
-}
-
-func searchRight(nums []int, target int) int {
-	left, right := 0, len(nums)-1
-	for right-left > 1 {
-		mid := (left + right) / 2
-		if nums[mid+1] > target && nums[mid] == target {
-			return mid
-		} else if nums[mid] <= target {
-			left = mid
-		} else {
-			right = mid
-		}
-	}
-
-	if nums[right] == target {
-		return right
-	}
-	if nums[left] == target {
-		return left
-	}
-	return -1
 }
