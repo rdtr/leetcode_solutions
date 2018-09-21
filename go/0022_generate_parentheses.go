e5be2589db9bbebeabc94a76a783af00141045cc
Norio Akagi <norakagi@amazon.com>
2018-09-21 11:43:10 -0700

    update 0022

--- go/0022_generate_parentheses.go
+++ go/0022_generate_parentheses.go
@@ -5,35 +5,23 @@ func generateParenthesis(n int) []string {
 		return []string{}
 	}
 
+	leftRemain, rightRemain := n-1, n
+	cur := []byte{'('}
 	var res []string
-	backtrack(n, 0, []byte{}, &res)
+	doGenerateParenthesis(leftRemain, rightRemain, cur, &res)
 	return res
 }
 
-// backtrack does the actual parenthesis adding,
-// leftCount means how many more left parenthesises should appear. This changes from 3 to 0.
-// rightCount means how many right parenthesises should appear. This starts from 0 and
-// incremented when leftCount is added, then finally this should be 0.
-func backtrack(leftCount int, rightCount int, curBytes []byte, res *[]string) {
-	// base case
-	if leftCount == 0 && rightCount == 0 {
-		*res = append(*res, string(curBytes))
+func doGenerateParenthesis(leftRemain, rightRemain int, cur []byte, res *[]string) {
+	if leftRemain == 0 && rightRemain == 0 {
+		*res = append(*res, string(cur))
 		return
 	}
 
-	curLen := len(curBytes)
-	if leftCount > 0 {
-		newBytes := make([]byte, curLen+1)
-		copy(newBytes[:curLen], curBytes)
-		newBytes[curLen] = '('
-
-		backtrack(leftCount-1, rightCount+1, newBytes, res)
+	if leftRemain > 0 {
+		doGenerateParenthesis(leftRemain-1, rightRemain, append(cur, '('), res)
 	}
-	if rightCount > 0 {
-		newBytes := make([]byte, curLen+1)
-		copy(newBytes[:curLen], curBytes)
-		newBytes[curLen] = ')'
-
-		backtrack(leftCount, rightCount-1, newBytes, res)
+	if rightRemain > leftRemain {
+		doGenerateParenthesis(leftRemain, rightRemain-1, append(cur, ')'), res)
 	}
 }
