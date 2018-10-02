d64c765795f450394b0612a42022290a0293081f
rdtr <redtree.dev1112@gmail.com>
2018-10-01 21:36:48 -0700

    add 0704

---
+++ go/0704_binary_search.go
@@ -0,0 +1,14 @@
+func search(nums []int, target int) int {
+	low, high := 0, len(nums)-1
+	for low <= high {
+		mid := low + (high-low)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
