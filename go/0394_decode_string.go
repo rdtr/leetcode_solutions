f4c0389d1035749e29fbcccc47e972a3990bd62c
rdtr <redtree.dev1112@gmail.com>
2018-10-04 00:39:12 -0700

    modify 0394

--- go/0394_decode_string.go
+++ go/0394_decode_string.go
@@ -1,65 +1,37 @@
-import (
-	"bytes"
-	"strconv"
-)
-
 func decodeString(s string) string {
-	res, _ := doDecodeString(s)
-	return res
-}
-
-func doDecodeString(s string) (string, int) {
-	slen := len(s)
-	if slen == 0 {
-		return "", 0
-	}
-
-	var res bytes.Buffer
-	for i := 0; i < slen; {
-		if !isNumber(s[i]) {
-			res.WriteByte(s[i])
-			i++
-			continue
+	stack := []*Item{&Item{}}
+	counting := false
+	var numStart int
+    
+	for i, ch := range s {
+		switch {
+		case ch >= '0' && ch <= '9':
+			if !counting {
+				var newBuf bytes.Buffer
+				item := &Item{Val: newBuf}
+				stack = append(stack, item)
+				numStart = i
+				counting = true
+			}
+		case ch == '[':
+            counting = false
+			numStr := s[numStart:i]
+			numInt64, _ := strconv.ParseInt(numStr, 10, 32)
+			stack[len(stack)-1].Num = int(numInt64)
+		case ch == ']':
+			tmpNum, tmpVal := stack[len(stack)-1].Num, stack[len(stack)-1].Val.Bytes()
+			stack = stack[:len(stack)-1]
+			for j := 0; j < tmpNum; j++ {
+				stack[len(stack)-1].Val.Write(tmpVal)
+			}
+		default:
+			stack[len(stack)-1].Val.WriteRune(ch)
 		}
-
-		var newRes string
-		newRes, i = extract(s, i)
-		res.WriteString(newRes)
 	}
-	return res.String(), 0
+	return stack[0].Val.String()
 }
 
-func extract(s string, i int) (string, int) {
-	var b bytes.Buffer
-
-	// extract number
-	var numBuf bytes.Buffer
-	for s[i] != '[' {
-		numBuf.WriteByte(s[i])
-		i++
-	}
-	i++ // skip '['
-
-	num, _ := strconv.ParseInt(numBuf.String(), 10, 32)
-	// extract string
-	for s[i] != ']' {
-		if isNumber(s[i]) {
-			newRes, newI := extract(s, i)
-			b.WriteString(newRes)
-			i = newI
-			continue
-		}
-		b.WriteByte(s[i])
-		i++
-	}
-
-	var res bytes.Buffer
-	for j := 0; j < int(num); j++ {
-		res.WriteString(b.String())
-	}
-	return res.String(), i + 1 // skip ']'
-}
-
-func isNumber(ch byte) bool {
-	return ch >= '0' && ch <= '9'
+type Item struct {
+	Num int
+	Val bytes.Buffer
 }
