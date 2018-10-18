ce6ee0dc67891d7f04463515cdc35f8938150acf
rdtr <redtree.dev1112@gmail.com>
2018-10-18 02:34:24 -0700

    update 0049

--- go/0049_group_anagrams.go
+++ go/0049_group_anagrams.go
@@ -1,39 +1,23 @@
-package main
-
-import "bytes"
-
 func groupAnagrams(strs []string) [][]string {
-	alphaBits := make([]string, len(strs))
-	for i, str := range strs {
-		alphaBits[i] = getAlphabetBit(str)
+	m := make(map[[26]int][]string)
+	for i := 0; i < len(strs); i++ {
+		str := strs[i]
+
+		var k [26]int
+		for j := 0; j < len(str); j++ {
+			k[(str[j]-'a')]++
+		}
+
+		if _, ok := m[k]; ok {
+			m[k] = append(m[k], str)
+		} else {
+			m[k] = []string{str}
+		}
 	}
 
 	var res [][]string
-	newIndex := 0
-	mp := make(map[string]int)
-	for bitIndex, alphaBit := range alphaBits {
-		if mapIndex, ok := mp[alphaBit]; !ok {
-			mp[alphaBit] = newIndex
-			res = append(res, []string{strs[bitIndex]})
-			newIndex++
-			continue
-		} else {
-			res[mapIndex] = append(res[mapIndex], strs[bitIndex])
-		}
+	for _, v := range m {
+		res = append(res, v)
 	}
 	return res
-}
-
-func getAlphabetBit(str string) string {
-	res := make([]int, 26)
-	for _, ch := range str {
-		chInt := int(ch - 'a')
-		res[chInt] += 1
-	}
-
-	var b bytes.Buffer
-	for _, bit := range res {
-		b.WriteByte(byte(int('0') + bit))
-	}
-	return b.String()
 }
