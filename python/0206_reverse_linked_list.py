65179fde7cd54b8b25fe99614b439375345b7cea
rdtr <redtree.dev1112@gmail.com>
2019-01-20 23:45:40 -0800

    add 0206 py

---
+++ python/0206_reverse_linked_list.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        if not head:
+            return
+
+        prev = None
+        cur = head
+        next = cur.next
+
+        while cur:
+            next = cur.next
+            cur.next = prev
+            prev, cur = cur, next
+
+        return prev
