e3b9c28b9f9d538d77115ab74aee9f366e560c65
rdtr <redtree.dev1112@gmail.com>
2019-06-26 12:13:28 -0700

    update 0003 py

--- python/0003_longest_substring_without_repeating_characters.py
+++ python/0003_longest_substring_without_repeating_characters.py
@@ -14,3 +14,32 @@ class Solution(object):
                 l = dist
             m[ch] = i
         return l
+
+
+from collections import defaultdict
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+
+        m = defaultdict(lambda: 0)
+        l = r = 0
+
+        maxSoFar = 0
+        while True:
+            while r < len(s) and m[s[r]] == 0:
+                m[s[r]] = 1
+                maxSoFar = max(maxSoFar, r - l + 1)
+                r += 1
+
+            if r >= len(s) - 1:
+                break
+
+            while l < len(s) and s[l] != s[r]:
+                m[s[l]] -= 1
+                l += 1
+            m[s[l]] -= 1
+            l += 1
+        return maxSoFar
