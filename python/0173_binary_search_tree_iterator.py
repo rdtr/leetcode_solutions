a8a7dfc883ba03ec3578c1b2eb060055c4713cd9
rdtr <redtree.dev1112@gmail.com>
2019-06-28 12:55:25 -0700

    update 0173 py

--- python/0173_binary_search_tree_iterator.py
+++ python/0173_binary_search_tree_iterator.py
@@ -1,33 +1,42 @@
-class BSTIterator(object):
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator:
+
     def __init__(self, root):
         """
         :type root: TreeNode
         """
-        self.stack = []
-        if not root:
-            return
-        while True:
-            self.stack.append(root)
-            if not root.left:
-                break
-            root = root.left
 
-    def hasNext(self):
-        """
-        :rtype: bool
-        """
-        return len(self.stack) > 0
+        self.stack = []
+        cur = root
+        while cur:
+            self.stack.append(cur)
+            cur = cur.left
 
     def next(self):
         """
+        @return the next smallest number
         :rtype: int
         """
-        node = self.stack.pop()
-        if node.right:
-            newnode = node.right
-            while True:
-                self.stack.append(newnode)
-                if not newnode.left:
-                    break
-                newnode = newnode.left
-        return node.val
+
+        res = self.stack.pop()
+        cur = res
+        if cur.right:
+            cur = cur.right
+            while cur:
+                self.stack.append(cur)
+                cur = cur.left
+        return res.val
+
+    def hasNext(self):
+        """
+        @return whether we have a next smallest number
+        :rtype: bool
+        """
+
+        return len(self.stack) > 0
