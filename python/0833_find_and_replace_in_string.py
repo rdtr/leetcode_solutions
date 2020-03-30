215061cdd558cbdd14414d3bc45c0be967cea8c2
Norio Akagi <norakagi@amazon.com>
2020-03-29 17:55:52 -0700

    update 0833 py

--- python/0833_find_and_replace_in_string.py
+++ python/0833_find_and_replace_in_string.py
@@ -1,14 +1,27 @@
 class Solution:
-    def findReplaceString(self, S, indexes, sources, targets):
-        """
-        :type S: str
-        :type indexes: List[int]
-        :type sources: List[str]
-        :type targets: List[str]
-        :rtype: str
-        """
+    def findReplaceString(self, S: str, indexes: List[int], sources: List[str], targets: List[str]) -> str:
+        replaces = [(x, y, z) for (x, y, z) in zip(indexes, sources, targets)]
+        replaces.sort(key=lambda x: x[0])
 
-        for index, source, target in sorted(zip(indexes, sources, targets), reverse=True):
-            if S[index:index + len(source)] == source:
-                S = S[:index] + target + S[index + len(source):]
-        return S
+        if not replaces:
+            return S
+
+        slen = len(S)
+        if slen == 0:
+            return S
+
+        res = []
+        c = 0
+        i = 0
+        while c < slen:
+            if i >= len(replaces) or c < replaces[i][0]:
+                res.append(S[c])
+                c += 1
+                continue
+
+            source, target = replaces[i][1], replaces[i][2]
+            if c + len(source) <= slen and S[c:c+len(source)] == source:
+                res.extend([t for t in target])
+                c += len(source)
+            i += 1
+        return ''.join(res)
