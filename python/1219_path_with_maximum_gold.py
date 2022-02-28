eec4b1759b54ac1d325244eab7efd6aaa50530c6
Norio Akagi <norioakagi@NorionoMacBook-Pro-2.local>
2022-02-28 01:48:05 -0800

    add 2013 py

--- python/1219_path_with_maximum_gold.py
+++ python/1219_path_with_maximum_gold.py
@@ -1,31 +1,32 @@
+from collections import deque
+
+
 class Solution:
-    def getMaximumGold(self, grid: List[List[int]]) -> int:
-        row = len(grid)
-        if row == 0:
-            return 0
-        col = len(grid[0])
-        if col == 0:
-            return 0
-        
-        res = [0]
-        visited = set()
-        for i in range(row):
-            for j in range(col):
-                if (i, j) not in visited:
-                    if grid[i][j] != 0:
-                        visited.add((i, j))
-                        self.helper(grid, i, j, visited, grid[i][j] , res)
-                        visited.remove((i, j))
-        return res[0]
-    
-    def helper(self, grid, i, j, visited, cursum, res):
-        if cursum > res[0]:
-            res[0] = cursum
-        
-        moves = [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]
-        for x, y in moves:
-            if x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0]) and grid[x][y] != 0:
-                if (x, y) not in visited:
-                    visited.add((x, y))
-                    self.helper(grid, x, y, visited, cursum + grid[x][y], res)
-                    visited.remove((x, y))
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        rowNum = len(grid)
+        if rowNum == 0:
+            return -1
+        colNum = len(grid[0])
+
+        que = deque([(0, 0, k)])
+        seen = set((0, 0, k))
+        stepNum = 0
+
+        while que:
+            qlen = len(que)
+            for i in range(qlen):
+                x, y, num = que.popleft()
+                if x == rowNum - 1 and y == colNum - 1:
+                    return stepNum
+
+                for nextX, nextY in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]:
+                    nextNum = num
+                    if nextX < 0 or nextX >= rowNum or nextY < 0 or nextY >= colNum:
+                        continue
+                    if grid[nextX][nextY] == 1:
+                        nextNum -= 1
+                    if nextNum >= 0 and (nextX, nextY, nextNum) not in seen:
+                        seen.add((nextX, nextY, nextNum))
+                        que.append((nextX, nextY, nextNum))
+            stepNum += 1
+        return -1
