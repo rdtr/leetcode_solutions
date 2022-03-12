51800d33c57e36ef62b6067d6f91a82c0e55dc6d
Norio Akagi <norioakagi@NorionoMacBook-Pro-2.local>
2022-03-12 01:20:46 -0800

    add 0450 py

---
+++ python/0450_delete_node_in_a_bst.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                nextNode = root.right
+                while nextNode.left:
+                    nextNode = nextNode.left
+                root.val = nextNode.val
+                root.right = self.deleteNode(root.right, nextNode.val)
+        return root
+
